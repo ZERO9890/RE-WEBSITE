8bc48115e8a49d7f1ad809d4ffa2ddcf66375964
Pranav Bhave <114678452+ZERO9890@users.noreply.github.com>
2025-08-29 08:53:05 +0530

    updated version of next.config.mjs to run

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',  // 🔹 required for GitHub Pages
+  basePath: isProd ? '/repo-name' : '', // 🔹 replace "repo-name" with your GitHub repo name
 }
 
 export default nextConfig
